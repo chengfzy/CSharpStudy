04ea192e35b9b284b8d48a8884e826341f6f106d
Jeffery Cheng <chengcheng0829@gmail.com>
2020-03-27 12:02:21 +0800

    init project

---
+++ Eg01_Basic/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using static System.Console;
+
+namespace Eg01_Basic
+{
+
+    class Person
+    {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        //
+        public string FullName => $"{FirstName} {LastName}";
+    }
+
+    public enum Color
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+    [Flags]
+    public enum DaysOfWeek
+    {
+        Monday = 0x1,
+        TuesDay = 0x2,
+        Wednesday = 0x4,
+        Thursday = 0x8,
+        Friday = 0x10,
+        Saturday = 0x20,
+        Sunday = 0x40,
+        Weekend = Saturday | Sunday,
+        Workday = 0x1f,
+        AllWeek = Workday | Weekend
+    }
+
+    class Program
+    {
+        static void classStudy()
+        {
+            Person person = new Person("Cheng", "Jeffery");
+            WriteLine($"Person is \"{person.FullName}\"");
+        }
+
+        static void enumStudy()
+        {
+            DaysOfWeek mondayAndWednesday = DaysOfWeek.Monday | DaysOfWeek.Wednesday;
+            WriteLine(mondayAndWednesday);
+            WriteLine(DaysOfWeek.Weekend);
+
+            foreach (var day in Enum.GetNames(typeof(Color)))
+            {
+                WriteLine(day);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            classStudy();
+            enumStudy();
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# C# Study Code using .NET Core
+
+## Contents
+1. Eg01_Basic
+    Basic usage, include
+    - Class
+    - Print `enum`, `[Flags]` options for enum
